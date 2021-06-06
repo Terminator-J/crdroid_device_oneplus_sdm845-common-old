d7aec1b6b7093688a6c0708eafa81032d4de1d54
Terminator-J <34873300+Terminator-J@users.noreply.github.com>
2021-06-06 10:44:45 -0700

    sdm845-common: Revert R-only commits from upstream
    Revert "sdm845-common: libcvface_api: Return non-null handle during tracker creation"
    
    This reverts commit 8509b74a311769bb496356bac236ceb4ef2fe7f2.
    
    Revert "sdm845-common: Stub out libcvface_api.so"
    
    This reverts commit de5ce2ecb09cc9540a91f48026b46dff1795882b.
    
    Revert "sdm845-common: Include VNDK 29 prebuilts"
    
    This reverts commit 98e7f3c82fcae48db051b20a90fde50936c97d43.
    
    Revert "sdm845-common: Add WifiOverlay"
    
    This reverts commit 5aba0f48622601b1199f86d586c28895c56b7751.
    
    Revert "sdm845-common: overlay: Remove wifi related internal overlays"
    
    This reverts commit 0d7a95c3b057ec2f1199f77a08e87b913ae0afee.
    
    Revert "sdm845-common: overlay: Update pinner list for new SystemUI location"
    
    This reverts commit 48c04410005f6a200e4a1fb28703842ec4b0a359.
    
    Revert "sdm845-common: Use libcutils-v29.so for libdpmframework.so"
    
    This reverts commit 8a85ee7c527389ca9227c0e1c305421177ee61bd.
    
    Revert "sdm845-common: Relocate aptX blobs to system_ext"
    
    This reverts commit 7e65fdbbc296d95a40dc0b887182fb38e1dd52e3.
    
    Revert "sdm845-common: Include QSSI sepolicy"
    
    This reverts commit ffad87fadcafe76016d5206f30e2c4d17a8c0af0.

--- common.mk
+++ common.mk
@@ -43,7 +43,6 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 29
-PRODUCT_EXTRA_VNDK_VERSIONS := $(PRODUCT_TARGET_VNDK_VERSION)
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -91,7 +90,6 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    libcvface_api \
     Snap
 
 # Common init scripts
@@ -210,15 +208,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
-
-# VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libcutils-v29.so \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcutils-v29.so
-
-# WiFi
-PRODUCT_PACKAGES += \
-    WifiOverlay
 
 # WiFi Display
 PRODUCT_PACKAGES += \
