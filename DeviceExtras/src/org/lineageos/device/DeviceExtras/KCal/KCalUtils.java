srgrusso/android_device_oneplus_sdm845-common

sdm845-common: Introduce DeviceExtras!
- Awesome alert slider configs
- Ambient display gestures (doze merged into DeviceExtras) + QS tile
- DC-Dimming + QS tile
- FPS Overlay + QS tile
- HBM & Auto HBM + QS tile
- Panel modes + QS tile
- Kcal color calibration + QS tile
- Force USB 2.0 FastCharge  + QS tile
- Vibration strength preferences

Co-authored-by: Max Weninger <max.weninger@gmail.com>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: AliHasan7671 <AHB7671@gmail.com>
Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
Co-authored-by: darkobas <darkobas@gmail.com>
Co-authored-by: kldoc <fred.berbigier@gmail.com>
Co-authored-bt: Nauval Rizky <enuma.alrizky@gmail.com>
Co-authored-by: Christian Oder <myself5@carbonrom.org>
Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
Co-authored-by: oddlyspaced <oddlyspaced1@gmail.com>
Co-authored-by: LuK1337 <priv.luk@gmail.com>
Co-authored-by: chrisw444 <wandersonrodriguesf1@gmail.com>
Co-authored-by: Adrian DC <radian.dc@gmail.com>
Co-authored-by: SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
Co-authored-by: Michele Bono <bono.michele94@gmail.com>
Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
Signed-off-by: AnierinB <anierin@evolution-x.org>

DeviceExtras/src/org/evolution/device/DeviceExtras/KCal/KCalUtils.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2018 The Xiaomi-SDM660 Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package org.evolution.device.DeviceExtras.kcal;
+
+public interface KCalUtils {
+    String PREF_ENABLED = "kcal_enabled";
+    String PREF_RED = "color_red";
+    String PREF_GREEN = "color_green";
+    String PREF_BLUE = "color_blue";
+    String PREF_SATURATION = "saturation";
+    String PREF_VALUE = "value";
+    String PREF_CONTRAST = "contrast";
+    String PREF_HUE = "hue";
+
+    int RED_DEFAULT = 256;
+    int GREEN_DEFAULT = 256;
+    int BLUE_DEFAULT = 256;
+    int SATURATION_DEFAULT = 127;
+    int SATURATION_OFFSET = 128;
+    int VALUE_DEFAULT = 127;
+    int VALUE_OFFSET = 128;
+    int CONTRAST_DEFAULT = 127;
+    int CONTRAST_OFFSET = 128;
+    int HUE_DEFAULT = 0;
+
+    String COLOR_FILE = "/sys/module/msm_drm/parameters";
+    String COLOR_FILE_RED = COLOR_FILE + "/kcal_red";
+    String COLOR_FILE_GREEN = COLOR_FILE + "/kcal_green";
+    String COLOR_FILE_BLUE = COLOR_FILE + "/kcal_blue";
+    String COLOR_FILE_SAT = COLOR_FILE + "/kcal_sat";
+    String COLOR_FILE_HUE = COLOR_FILE + "/kcal_hue";
+    String COLOR_FILE_VAL = COLOR_FILE + "/kcal_val";
+    String COLOR_FILE_CONT = COLOR_FILE + "/kcal_cont";
+}

DeviceExtras/src/org/evolution/device/DeviceExtras/ModeSwitch/DCModeSwitch.java
@@ -1,0 +1,53 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.device.DeviceExtras;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import org.evolution.device.DeviceExtras.DeviceExtras;
+
+public class DCModeSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/dimlayer_bl_en";
+
+    public static String getFile() {
+        if (FileUtils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return FileUtils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        FileUtils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}

DeviceExtras/src/org/evolution/device/DeviceExtras/ModeSwitch/SRGBModeSwitch.java
@@ -1,0 +1,51 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.device.DeviceExtras;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+public class SRGBModeSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/native_display_srgb_color_mode";
+
+    public static String getFile() {
+        if (FileUtils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return FileUtils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        FileUtils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}

DeviceExtras/src/org/evolution/device/DeviceExtras/SecureSettingsStore.java
@@ -1,0 +1,58 @@
+/*
+ * Copyright (C) 2017 AICP
+ * Copyright (C) 2018 CarbonROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.evolution.device.DeviceExtras;
+
+import android.content.ContentResolver;
+import android.preference.PreferenceDataStore;
+import android.provider.Settings;
+
+public class SecureSettingsStore extends androidx.preference.PreferenceDataStore
+        implements PreferenceDataStore {
+
+    private final ContentResolver mContentResolver;
+
+    public SecureSettingsStore(ContentResolver contentResolver) {
+        mContentResolver = contentResolver;
+    }
+
+    public boolean getBoolean(String key, boolean defValue) {
+        return getInt(key, defValue ? 1 : 0) != 0;
+    }
+
+    public int getInt(String key, int defValue) {
+        return Settings.Secure.getInt(mContentResolver, key, defValue);
+    }
+
+    public String getString(String key, String defValue) {
+        String result = Settings.Secure.getString(mContentResolver, key);
+        return result == null ? defValue : result;
+    }
+
+    public void putBoolean(String key, boolean value) {
+        putInt(key, value ? 1 : 0);
+    }
+
+    public void putInt(String key, int value) {
+        Settings.Secure.putInt(mContentResolver, key, value);
+    }
+
+    public void putString(String key, String value) {
+        Settings.Secure.putString(mContentResolver, key, value);
+    }
+
+}

DeviceExtras/src/org/evolution/device/DeviceExtras/Services/USB2FastChargeTileService.java
@@ -1,0 +1,75 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+* Copyright (C) 2021 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.device.DeviceExtras;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+
+import org.evolution.device.DeviceExtras.DeviceExtras;
+
+@TargetApi(24)
+public class USB2FastChargeTileService extends TileService {
+    private boolean enabled = false;
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        enabled = USB2FastChargeModeSwitch.isCurrentlyEnabled(this);
+        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        getQsTile().updateTile();
+
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        enabled = USB2FastChargeModeSwitch.isCurrentlyEnabled(this);
+        FileUtils.writeValue(USB2FastChargeModeSwitch.getFile(), enabled ? "0" : "1");
+        sharedPrefs.edit().putBoolean(DeviceExtras.KEY_USB2_SWITCH, enabled ? false : true).commit();
+        //getQsTile().setLabel(enabled ? "USB 2.0 FC off" : "USB 2.0 FC On");
+        getQsTile().setState(enabled ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
+        getQsTile().updateTile();
+    }
+}

DeviceExtras/src/org/evolution/device/DeviceExtras/Slider/RingerController.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2018-2021 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.evolution.device.DeviceExtras.slider;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.util.Log;
+import android.util.SparseIntArray;
+
+import org.evolution.device.DeviceExtras.Constants;
+import org.evolution.device.DeviceExtras.SliderControllerBase;
+
+public final class RingerController extends SliderControllerBase {
+
+    public static final int ID = 5;
+
+    private static final String TAG = "RingerController";
+
+    private static final int RINGER_NORMAL = 50;
+    private static final int RINGER_VIBRATE = 51;
+    private static final int RINGER_SILENT = 52;
+
+    private static final SparseIntArray MODES = new SparseIntArray();
+    static {
+        MODES.put(RINGER_NORMAL, AudioManager.RINGER_MODE_NORMAL);
+        MODES.put(RINGER_VIBRATE, AudioManager.RINGER_MODE_VIBRATE);
+        MODES.put(RINGER_SILENT, AudioManager.RINGER_MODE_SILENT);
+    }
+
+    private final AudioManager mAudioManager;
+
+    public RingerController(Context context) {
+        super(context);
+        mAudioManager = context.getSystemService(AudioManager.class);
+    }
+
+    @Override
+    protected int processAction(int action) {
+        Log.d(TAG, "slider action: " + action);
+        if (MODES.indexOfKey(action) >= 0) {
+            mAudioManager.setRingerModeInternal(MODES.get(action));
+            switch (action) {
+                case RINGER_NORMAL:
+                    return Constants.MODE_RING;
+                case RINGER_VIBRATE:
+                    return Constants.MODE_VIBRATE;
+                case RINGER_SILENT:
+                    return Constants.MODE_SILENT;
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+        mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+    }
+}

Terminator-J/crdroid_device_oneplus_sdm845-common-old

DeviceExtras/src/org/lineageos/device/DeviceExtras/KCal/KCalUtils.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2018 The Xiaomi-SDM660 Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package org.lineageos.device.DeviceExtras.kcal;
+
+public interface KCalUtils {
+    String PREF_ENABLED = "kcal_enabled";
+    String PREF_RED = "color_red";
+    String PREF_GREEN = "color_green";
+    String PREF_BLUE = "color_blue";
+    String PREF_SATURATION = "saturation";
+    String PREF_VALUE = "value";
+    String PREF_CONTRAST = "contrast";
+    String PREF_HUE = "hue";
+
+    int RED_DEFAULT = 256;
+    int GREEN_DEFAULT = 256;
+    int BLUE_DEFAULT = 256;
+    int SATURATION_DEFAULT = 127;
+    int SATURATION_OFFSET = 128;
+    int VALUE_DEFAULT = 127;
+    int VALUE_OFFSET = 128;
+    int CONTRAST_DEFAULT = 127;
+    int CONTRAST_OFFSET = 128;
+    int HUE_DEFAULT = 0;
+
+    String COLOR_FILE = "/sys/module/msm_drm/parameters";
+    String COLOR_FILE_RED = COLOR_FILE + "/kcal_red";
+    String COLOR_FILE_GREEN = COLOR_FILE + "/kcal_green";
+    String COLOR_FILE_BLUE = COLOR_FILE + "/kcal_blue";
+    String COLOR_FILE_SAT = COLOR_FILE + "/kcal_sat";
+    String COLOR_FILE_HUE = COLOR_FILE + "/kcal_hue";
+    String COLOR_FILE_VAL = COLOR_FILE + "/kcal_val";
+    String COLOR_FILE_CONT = COLOR_FILE + "/kcal_cont";
+}

DeviceExtras/src/org/lineageos/device/DeviceExtras/ModeSwitch/DCModeSwitch.java
@@ -1,0 +1,53 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.lineageos.device.DeviceExtras;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import org.lineageos.device.DeviceExtras.DeviceExtras;
+
+public class DCModeSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/dimlayer_bl_en";
+
+    public static String getFile() {
+        if (FileUtils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return FileUtils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        FileUtils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}

DeviceExtras/src/org/lineageos/device/DeviceExtras/ModeSwitch/SRGBModeSwitch.java
@@ -1,0 +1,51 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.lineageos.device.DeviceExtras;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+public class SRGBModeSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/native_display_srgb_color_mode";
+
+    public static String getFile() {
+        if (FileUtils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return FileUtils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        FileUtils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}

DeviceExtras/src/org/lineageos/device/DeviceExtras/SecureSettingsStore.java
@@ -1,0 +1,58 @@
+/*
+ * Copyright (C) 2017 AICP
+ * Copyright (C) 2018 CarbonROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.device.DeviceExtras;
+
+import android.content.ContentResolver;
+import android.preference.PreferenceDataStore;
+import android.provider.Settings;
+
+public class SecureSettingsStore extends androidx.preference.PreferenceDataStore
+        implements PreferenceDataStore {
+
+    private final ContentResolver mContentResolver;
+
+    public SecureSettingsStore(ContentResolver contentResolver) {
+        mContentResolver = contentResolver;
+    }
+
+    public boolean getBoolean(String key, boolean defValue) {
+        return getInt(key, defValue ? 1 : 0) != 0;
+    }
+
+    public int getInt(String key, int defValue) {
+        return Settings.Secure.getInt(mContentResolver, key, defValue);
+    }
+
+    public String getString(String key, String defValue) {
+        String result = Settings.Secure.getString(mContentResolver, key);
+        return result == null ? defValue : result;
+    }
+
+    public void putBoolean(String key, boolean value) {
+        putInt(key, value ? 1 : 0);
+    }
+
+    public void putInt(String key, int value) {
+        Settings.Secure.putInt(mContentResolver, key, value);
+    }
+
+    public void putString(String key, String value) {
+        Settings.Secure.putString(mContentResolver, key, value);
+    }
+
+}

DeviceExtras/src/org/lineageos/device/DeviceExtras/Services/USB2FastChargeTileService.java
@@ -1,0 +1,75 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+* Copyright (C) 2021 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.lineageos.device.DeviceExtras;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+
+import org.lineageos.device.DeviceExtras.DeviceExtras;
+
+@TargetApi(24)
+public class USB2FastChargeTileService extends TileService {
+    private boolean enabled = false;
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        enabled = USB2FastChargeModeSwitch.isCurrentlyEnabled(this);
+        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        getQsTile().updateTile();
+
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        enabled = USB2FastChargeModeSwitch.isCurrentlyEnabled(this);
+        FileUtils.writeValue(USB2FastChargeModeSwitch.getFile(), enabled ? "0" : "1");
+        sharedPrefs.edit().putBoolean(DeviceExtras.KEY_USB2_SWITCH, enabled ? false : true).commit();
+        //getQsTile().setLabel(enabled ? "USB 2.0 FC off" : "USB 2.0 FC On");
+        getQsTile().setState(enabled ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
+        getQsTile().updateTile();
+    }
+}

DeviceExtras/src/org/lineageos/device/DeviceExtras/Slider/RingerController.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2018-2021 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.device.DeviceExtras.slider;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.util.Log;
+import android.util.SparseIntArray;
+
+import org.lineageos.device.DeviceExtras.Constants;
+import org.lineageos.device.DeviceExtras.SliderControllerBase;
+
+public final class RingerController extends SliderControllerBase {
+
+    public static final int ID = 5;
+
+    private static final String TAG = "RingerController";
+
+    private static final int RINGER_NORMAL = 50;
+    private static final int RINGER_VIBRATE = 51;
+    private static final int RINGER_SILENT = 52;
+
+    private static final SparseIntArray MODES = new SparseIntArray();
+    static {
+        MODES.put(RINGER_NORMAL, AudioManager.RINGER_MODE_NORMAL);
+        MODES.put(RINGER_VIBRATE, AudioManager.RINGER_MODE_VIBRATE);
+        MODES.put(RINGER_SILENT, AudioManager.RINGER_MODE_SILENT);
+    }
+
+    private final AudioManager mAudioManager;
+
+    public RingerController(Context context) {
+        super(context);
+        mAudioManager = context.getSystemService(AudioManager.class);
+    }
+
+    @Override
+    protected int processAction(int action) {
+        Log.d(TAG, "slider action: " + action);
+        if (MODES.indexOfKey(action) >= 0) {
+            mAudioManager.setRingerModeInternal(MODES.get(action));
+            switch (action) {
+                case RINGER_NORMAL:
+                    return Constants.MODE_RING;
+                case RINGER_VIBRATE:
+                    return Constants.MODE_VIBRATE;
+                case RINGER_SILENT:
+                    return Constants.MODE_SILENT;
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+        mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+    }
+}