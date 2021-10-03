topes1/device_oneplus_sdm845-common

sdm845-common: DeviceExtras: Add earpiece/microphone gain preferences
Signed-off-by: AnierinB <anierin@evolution-x.org>

DeviceExtras/src/org/spark/device/DeviceExtras/Startup.java
@@ -35,8 +35,10 @@ public class Startup extends BroadcastReceiver {
     public void onReceive(final Context context, final Intent bootintent) {
 
         DeviceExtras.restoreSliderStates(context);
+        EarGainModeSwitch.restore(context);
         org.spark.device.DeviceExtras.doze.DozeUtils.checkDozeService(context);
         org.spark.device.DeviceExtras.kcal.KCalSettings.restore(context);
+        MicGainModeSwitch.restore(context);
         VibratorCallStrengthPreference.restore(context);
         VibratorNotifStrengthPreference.restore(context);
         VibratorStrengthPreference.restore(context);

Terminator-J/crdroid_device_oneplus_sdm845-common-old

DeviceExtras/src/org/lineageos/device/DeviceExtras/Startup.java
@@ -35,8 +35,10 @@ public class Startup extends BroadcastReceiver {
     public void onReceive(final Context context, final Intent bootintent) {
 
         DeviceExtras.restoreSliderStates(context);
+        EarGainPreference.restore(context);
         org.lineageos.device.DeviceExtras.doze.DozeUtils.checkDozeService(context);
         org.lineageos.device.DeviceExtras.kcal.KCalSettings.restore(context);
+        MicGainPreference.restore(context);
         VibratorCallStrengthPreference.restore(context);
         VibratorNotifStrengthPreference.restore(context);
         VibratorStrengthPreference.restore(context);