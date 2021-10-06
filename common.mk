c3f7cfa309cbe9f5185f77fac700b4be2102aa4d
Terminator-J <terminator_j@hotbutteredmonkey.com>
2021-10-06 12:37:06 -0700

    sdm845-common: selinux: ignore neverallows
    We're building userdebug, because if we build user then everything
    else breaks anyway, and we can't use "sepolicy_vndr" instead
    because we don't have a proper source-built vendor bringup yet.

--- common.mk
+++ common.mk
@@ -160,6 +160,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     RemovePackages
 
+# Selinux - We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
