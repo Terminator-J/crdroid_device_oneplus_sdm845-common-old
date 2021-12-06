e65098b92262852e818391ad0d424f39c04f242d
Terminator-J <terminator_j@hotbutteredmonkey.com>
2021-12-05 22:56:45 -0800

    sdm845-common: S bringup: Revert "Use libcutils-v29.so for libdpmframework.so"
    No longer allowed to have ELF binaries in PRODUCT_COPY_FILES,
    to be replaced with lib shim.
    
    This reverts commit 8a85ee7c527389ca9227c0e1c305421177ee61bd.

--- common.mk
+++ common.mk
@@ -225,11 +225,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.impl.oneplus_sdm845 \
     vendor.qti.hardware.vibrator.service.oneplus_sdm845
 
-# VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libcutils-v29.so \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcutils-v29.so
-
 # WiFi
 PRODUCT_PACKAGES += \
     WifiOverlay
