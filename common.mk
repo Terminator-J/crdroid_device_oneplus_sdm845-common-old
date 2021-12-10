96beeb2ff39d0800d76b1377df24df95040c4fbe
Terminator-J <terminator_j@hotbutteredmonkey.com>
2021-12-09 19:49:21 -0800

    sdm845-common: vendor: Prep for source-built vendor
    Revert "sdm845-common: Use media profiles from /vendor directly"
    
    This reverts commit 56557cedd32047b916b41a1c93d48240350704f9.

--- common.mk
+++ common.mk
@@ -146,6 +146,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lineage.livedisplay@2.1-service.oneplus_sdm845
 
+# Media
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_profiles_vendor.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles_vendor.xml
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
