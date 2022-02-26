f87fb28103b74025ce79df5d14e2b66bb3841c20
Terminator-J <terminator_j@hotbutteredmonkey.com>
2022-02-26 01:26:51 -0800

    sdm845-common: packages: Implement DeviceSettings
    Bringing it back, because why not...
    Needed the KeyHandler anyway for alert slider
    notification bubble broadcasts.
    
    Might as well put some stuff back in here, like
    multiple vibration modes & AutoHBM/threshold
    controls (better than LiveDisplay version).
    
    Co-authored-by: Max Weninger <max.weninger@gmail.com>
    Co-authored-by: Vachounet <vachounet@live.fr>
    Co-authored-by: kldoc <fred.berbigier@gmail.com>
    Co-authored-by: Christian Oder <myself5@carbonrom.org>
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
    Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Lup Gabriel <gwolf2u.website@gmail.com>
    Co-authored-by: firebird11 <hbgassel@gmail.com>
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    
    Change-Id: Ied60bf6cae5fe82252140f5e6d475a086dabf9da

--- common.mk
+++ common.mk
@@ -171,6 +171,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.class_main.sh \
+    init.devicesettings.rc \
     init.oem.rc \
     init.qcom.class_core.sh \
     init.qcom.early_boot.sh \
@@ -187,6 +188,10 @@ PRODUCT_PACKAGES += \
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
+# Device Settings
+PRODUCT_PACKAGES += \
+    DeviceSettings
 
 # Display
 PRODUCT_PACKAGES += \
@@ -401,7 +406,6 @@ PRODUCT_PACKAGES += \
 
 # tri-state-key
 PRODUCT_PACKAGES += \
-    KeyHandler \
     tri-state-key_daemon.vendor
 
 # Trust HAL
